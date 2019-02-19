05e42d9e37767094d89d4af1cfafe9309c350cd3
nam1956 <47291269+nam1956@users.noreply.github.com>
2019-02-20 00:42:24 +0900

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>WEB1 - Welcome</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Nosifer|Roboto+Condensed|Song+Myung" rel="stylesheet"> 
+</head>
+
+<body>
+    <h1><a href="index.html">WEB</a></h1>
+    <ol class="index">
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JavaScript</a></li>
+    </ol>
+    <hr style="color: blue">
