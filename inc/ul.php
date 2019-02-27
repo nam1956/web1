2c853fd207b77feac5a987b1a0eab5813dbbc33b
nam1956 <47291269+nam1956@users.noreply.github.com>
2019-02-27 21:27:20 +0900

    Add files via upload

---
+++ inc/ul.php
@@ -0,0 +1,6 @@
+<ol class="index">
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JavaScript</a></li>
+    </ol>
+    <hr style="color: blue">
